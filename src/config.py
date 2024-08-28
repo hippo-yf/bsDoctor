b6cc6b0a12670cdd39fb0205f3aba80f43fbe056
hippo-yf <68546557+hippo-yf@users.noreply.github.com>
2024-08-28 20:45:35 +0000

    update args

--- src/config.py
+++ src/config.py
@@ -17,64 +17,12 @@ params = dict()
 global data
 data = {}
 
-# class Parameters():
-#     fafile: str
-#     fa: MyFastaFile
-#     bamfile: str
-#     # out_atcg: str
-#     # out_cg: str
-#     # out_bed: str
-#     # chr: str
-#     start: int
-#     end: int
-#     step: int
-#     quality_threshold: int
-#     context_size: int
-#     cgkmer: int
-#     coordinate_base: int  # 0/1-based
-#     swap_strand: bool # swap the counts of forward and reverse strands
-#     read_quality: int
-
-#     chr_MT: str
-#     chr_lambda: str
-#     chr_plastid: str
-#     testchrs: str
-#     binSize:int # bin length
-#     bins_lambda: int # #bins
-#     bins_MT: int
-#     MAXDEPTH: int # max depth for counting
-    
-#     # gene pading for pan-gene summary
-#     gene_padding: int # up-/down-stream
-#     gene_breaks: int # breaks of up body down
-
-#     # read sampling
-#     reads_to_sample: int
-#     reads_sample_window: int # bp
-#     max_reads_per_batch: int
-
-#     # pangene meth
-#     PANGENE_SAMPLED: int
-
-#     binSize_lambda: Tuple
-#     binSize_MT: Tuple
-#     binSizeContig: Dict
-#     binsContig: Dict
-#     chrs_alias: Dict
-
-
-
 
 ##############################################
 
 # def config_params(args: NamedTuple):
 def config_params(options: Namespace = Namespace()) -> None:
     
-    # copy args into "params"
-    for key, value in options.__dict__.items():
-        params[key] = value
-        # pass
-
     fafile = 'genome/hg38.fa'
     # fafile = 'genome/GCF_000001735.4_TAIR10.1_genomic.fna'
     bamfile = 'data/ENCFF873NOV.bam'
@@ -102,6 +50,7 @@ def config_params(options: Namespace = Namespace()) -> None:
     binSize = 100_000 # bin length
     bins_lambda = 1000 # #bins
     bins_MT = 1000
+    bins_plastid = 1000
 
     # max depth for counting
     # depth = max{depth, MAXDEPTH}
@@ -112,7 +61,7 @@ def config_params(options: Namespace = Namespace()) -> None:
     gene_breaks = 50 # breaks of up body down
 
     # quality read sampling
-    reads_to_sample = 100_00
+    reads_to_sample = 10_000
     # reads_to_sample = 100_000
     reads_sample_window = 10 # bp
     max_reads_per_batch = 500
@@ -126,7 +75,7 @@ def config_params(options: Namespace = Namespace()) -> None:
 
     params['start'] = 0
     params['end'] = math.inf
-    params['step'] = 2000
+    # params['step'] = 2000
     params['quality_threshold'] = 0
     params['read_quality'] = 0
     params['nuclear_sampling_step'] = 1000
@@ -142,17 +91,24 @@ def config_params(options: Namespace = Namespace()) -> None:
     params['binSize'] = binSize
     params['bins_lambda'] = bins_lambda
     params['bins_MT'] = bins_MT
+    params['bins_plastid'] = bins_plastid
     params['MAXDEPTH'] = MAXDEPTH
     # params['meth_bins'] = 20 #  #bins of meth
+    
+    # pangene
+    params['PANGENE_SAMPLED'] = PANGENE_SAMPLED
     params['gene_padding'] = gene_padding
     params['gene_breaks'] = gene_breaks
+
+    # quality
     params['reads_to_sample'] = reads_to_sample
     params['reads_sample_window'] = reads_sample_window
     params['max_reads_per_batch'] = max_reads_per_batch
-    params['PANGENE_SAMPLED'] = PANGENE_SAMPLED
-    params['MAX_COORDINATE'] = 2**63-1
+
     params['MAX_DP_BY_FIG'] = 30 # max dp for which figures plotted by each dp
     params['MAX_DP_CG_MOTIF'] = 50
+
+    params['MAX_COORDINATE'] = 2**63-1 # default end of chrs
     params['work_dir'] = '.'
     # params['report_dir'] = os.path.join(params['work_dir'], 'report')
     params['report_dir'] = params['work_dir']
@@ -162,68 +118,47 @@ def config_params(options: Namespace = Namespace()) -> None:
     myMakeDirs(params['report_dir'])
     myMakeDirs(params['img_dir'])
 
+    # to analyse or not
+    params['include_mt'] = False if options.chr_MT == '-' else True
+    params['include_plastid'] = False if options.chr_plastid == '-' else True
+    params['include_lambda'] = False if options.chr_lambda == '-' else True
+
+    #### copy/substitue args
+    for key, value in options.__dict__.items():
+        params[key] = value
+        # pass
+
     return None
-
-
-    # params = Parameters(
-    #     fafile=fafile, 
-    #     fa=None,
-    #     bamfile=bamfile,
-    #     # out_atcg='', # three types of outputs
-    #     # out_cg='',
-    #     # out_bed='',
-    #     # chr='chr20',  # 'all' for all chrs
-    #     start=0,
-    #     end=math.inf,
-    #     # end=50_000_000,
-    #     step=2000,
-    #     quality_threshold=0, # base seq quality
-    #     read_quality=0, # read mapping quality threshold
-    #     context_size=3, # size of CHG/...
-    #     cgkmer=6,
-    #     coordinate_base=1, # 0/1-based
-    #     swap_strand=False,  # swap counts of two strands
-
-    #     chr_MT = chr_MT,
-    #     chr_lambda = chr_lambda,
-    #     chr_plastid = chr_plastid,
-    #     testchrs = testchrs,
-    #     binSize = binSize,
-    #     bins_lambda = bins_lambda,
-    #     bins_MT = bins_MT,
-    #     MAXDEPTH = MAXDEPTH,
-    #     gene_padding = gene_padding,
-    #     gene_breaks = gene_breaks,
-    #     reads_to_sample = reads_to_sample,
-    #     reads_sample_window = reads_sample_window,
-    #     max_reads_per_batch = max_reads_per_batch,
-    #     PANGENE_SAMPLED = PANGENE_SAMPLED,
-    #     binSize_lambda = None,
-    #     binSize_MT = None,
-    #     binSizeContig = None,
-    #     binsContig = None,
-    #     chrs_alias = None
-    #     )
-
 
 
 class MyArgumentParser(ArgumentParser):
     def __init__(self, **args) -> None:
         super().__init__(**args)
 
-        self.add_argument('-i', '--atcg-file', dest='infile', help='an input .ATCGmap[.gz] file, default: read from stdin', type=str, required=False, default="-")
-        self.add_argument('-o', '--output-prefix', dest='outprefix', help='prefix of output files, a prefix.snv.gz and a prefix.vcf.gz will be returned, by default, same with input filename except suffix, for example, for input of path/sample.atcg.gz, the output is path/sample.snv.gz and path/sample.vcf.gz which is equivalent to setting -o path/sample', type=str)
-        self.add_argument('-m', '--mutation-rate', dest='mutation_rate', help='mutation rate a hyploid base', type=float, default=0.001)
-        self.add_argument('-e', '--error-rate', dest='error_rate', help='error rate a base is mis-detected due to sequencing or mapping', type=float, default=0.03)
-        self.add_argument('-c', '--methy-cg', dest='methy_cg', help='Cytosine methylation rate of CpG-context', type=float, default=0.6)
-        self.add_argument('-n', '--methy-ch', dest='methy_ncg', help='Cytosine methylation rate of non-CpG-context', type=float, default=0.01)
-        self.add_argument('-d', '--min-depth', dest='min_depth', help='sites with coverage depth less than this value will be skipped', type=int, default=10)
-        self.add_argument('-p', '--pvalue', dest='pvalue', help='p-value threshold', type=float, default=0.01)
-        self.add_argument('--shrink-depth', dest='shrink_depth', help='sites with coverage greater than this value will be shrinked by a square-root transform', type=int, default=60)
-        self.add_argument('--batch-size', dest='batch_size', help='a batch of sites will be processed at the same time', type=int, default=10000)
-        self.add_argument('-P', '--num-process', dest='num_process', help='number of processes in parallel', type=int, default=4)
-        self.add_argument('--pool-lower-num', dest='pool_lower_num', help='lower number of bacthes in memory pool per process', type=int, default=10)
-        self.add_argument('--pool-upper-num', dest='pool_upper_num', help='upper number of bacthes in memory pool per process', type=int, default=30)
-        self.add_argument('--keep-order', dest='keep_order', help='keep the results same order with input, true/false, or yes/no', type=as_bool, default=True)
+        self.add_argument('-b', '--bam-file', dest='bamfile', help='a .bam file', type=str, required=True)
+        self.add_argument('-f', '--fa-file', dest='fafile', help='a .fa[.gz] file of referece genome', type=str, required=True)
+        self.add_argument('-g', '--gtf-file', dest='gtffile', help='a .gtf[.gz] file', type=str, required=True)
+        self.add_argument('--chr', dest='testchrs', help='nuclear chromosomes to diagnose, seprated by comma(,), such as "chr1,chr2,chr3", all chromosomes by defaults', type=str, required=False, default='all')
+        self.add_argument('--mt', dest='chr_MT', help='name of mitochondrial DNA', type=str, required=False, default='-')
+        self.add_argument('--plastid', dest='chr_plastid', help='name of plastid DNA', type=str, required=False, default='-')
+        self.add_argument('--control-DNA', dest='chr_lambda', help='name of spiked-in DNA, usually lambda DNA', type=str, required=False, default='-')
+        self.add_argument('--sampling-step', dest='nuclear_sampling_step', help='sampling step size of nuclear chromosomes, 1Kbp by defaults', type=int, required=False, default=1000)
+        self.add_argument('--sampling-spacing', dest='nuclear_sampling_spacing', help='sampling spacing size of nuclear chromosomes, 10Kbp by defaults', type=int, required=False, default=10_000)
+        self.add_argument('--bin-size', dest='binSize', help='bin size of nuclear chromosomes, 100kbp by defaults', type=int, default=100_000)
+        self.add_argument('--bins-control', dest='bins_lambda, 1000 bins by defaults', help='bins of spkied-in control DNA', type=int, default=1000)
+        self.add_argument('--bins-mt', dest='bins_MT', help='bins of mitochondrial DNA, 1000 bins by defaults', type=int, default=1000)
+        self.add_argument('--bins-plastid', dest='bins_plastid', help='bins of plastid DNA, 1000 bins by defaults', type=int, default=1000)
+        self.add_argument('--base-quality', dest='quality_threshold', help='base quality threshold', type=int, required=False, default=0)
+        self.add_argument('--read-quality', dest='read_quality', help='read mapping quality threshold', type=int, required=False, default=0)
+        self.add_argument('--max-depth', dest='MAXDEPTH', help='depth larger than max depth will be truncated, 200 by defaults', type=int, default=200)
+        self.add_argument('--reads-for-quality', dest='reads_to_sample', help='reads to sample for quality statstics, 10K reads by defaults', type=int, default=10_000)
+        self.add_argument('--num-pangene', dest='PANGENE_SAMPLED', help='genes to sample for pangene methylaion level, 1000 by defaults', type=int, default=1_000)
+        self.add_argument('--max-depth-plot', dest='MAX_DP_BY_FIG', help='max depth for figures plotted for sites of each specific depth, 30 by defaults', type=int, default=30)
+        self.add_argument('--max-depth-motig', dest='MAX_DP_CG_MOTIF', help='max depth in CpG-motif diagnosis, 50 by defaults', type=int, default=50)
+        self.add_argument('--coordinate-base', dest='coordinate_base', help='0/1-based coordinate of output', type=int, default=1)
+        self.add_argument('--swap-strand', dest='swap_strand', help='swap read counts on two strands, true/false, or yes/no', type=as_bool, required=False, default='no')
+        self.add_argument('--report-dir', dest='report_dir', help='report directory, the current directory by defaults', type=str, default='.')
+        self.add_argument('--figure-dir', dest='img_dir', help='figure subdirectory', type=str, default='img')
+        ## todo
+        self.add_argument('--save-svg', dest='save_svg', help='save .svg figures or not, yes by defaults', type=as_bool, default='yes')
 
-        
